03820977a0a8a338ae502a556fccb552f4f57854
itsmefsd <itismefsd@gmail.com>
2025-07-30 18:05:27 +0000

    html and javascript element

---
+++ 08basicproject/src/App.jsx
@@ -0,0 +1,18 @@
+import { createElement, useState } from 'react'
+
+
+// function App(){
+
+// return createElement ("h1", {className: "myclass"}, "Hello")               // HTML create by JavaScript
+
+// }
+function App(){
+
+return (
+
+  <h1 className='myclass'>Hello</h1>                                         // HTML directly write
+)
+
+}
+
+export default App
