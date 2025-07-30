f3ef4da22db42b450875f5b848d919ec6233e06e
itsmefsd <itismefsd@gmail.com>
2025-07-30 17:05:15 +0000

    Multiple export

---
+++ 05basicproject/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DemoExport, UserComponent } from './UserComponent'
+
+function App() {
+  return (
+    <div>
+      <h1>Named Export in React</h1>
+      <UserComponent/>
+      <DemoExport/>
+    </div>
+  )
+}
+
+export default App
---
+++ 05basicproject/src/UserComponent.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function UserComponent() {
+  return (
+    <div>
+      <h1>Login Here</h1>
+    </div>
+  )
+}
+
+
+
+function DemoExport() {
+  return (
+    <div>
+      <h1>Demo Export</h1>
+    </div>
+  )
+}
+
+
+
+export {UserComponent, DemoExport}
